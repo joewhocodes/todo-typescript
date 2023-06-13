42c98ddb38bc54a39ca4c37e1d467fd412794e87
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-13 09:44:32 +0200

    add TodoList component, map TodoListItem inside and replace TodoListItem with TodoList in app.tsx

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 interface Props {
 	todo: Todo;
-    toggleTodo: ToggleTodo;
+	toggleTodo: ToggleTodo;
 }
 
 export const TodoListItem: React.FC<Props> = ({ todo, toggleTodo }) => {
-    return (
-      <li>
-        <label
-          style={{ textDecoration: todo.complete ? 'line-through' : undefined }}
-        >
-          <input type="checkbox" checked={todo.complete} onClick={() => {toggleTodo(todo)}}/> {todo.text}
-        </label>
-      </li>
-    );
-  };
+	return (
+		<li>
+			<label
+				style={{
+					textDecoration: todo.complete ? 'line-through' : undefined,
+				}}>
+				<input
+					type='checkbox'
+					checked={todo.complete}
+					onClick={() => {
+						toggleTodo(todo);
+					}}
+				/>{' '}
+				{todo.text}
+			</label>
+		</li>
+	);
+};
