c658342fe778a6811d64a2c3fdea53b2ce7decd4
Joseph Ulyatt <joeulyatt1@hotmail.co.uk>
2023-06-13 09:26:07 +0200

    add todo list item with hardcoded props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoListItem } from './TodoListItem';
+
+const todos: Todo[] = [
+	{
+		text: 'Feed the cat',
+		complete: false,
+	},
+	{
+		text: 'Wash up',
+		complete: true,
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ul>
+			<TodoListItem todo={todos[0]} />
+			<TodoListItem todo={todos[1]} />
+		</ul>
+	);
 }
 
 export default App;
